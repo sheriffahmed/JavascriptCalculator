7354e03d6a838ee5c280bfe4dd458db102ef5267
dannyxboi <danielorrego02@gmail.com>
2017-04-11 20:55:45 -0400

    init files

---
+++ JavascriptCalculator/index.js
@@ -0,0 +1,21 @@
+function add(){
+	// console.log("let's add");
+	// var math =
+	var math = 4 + 7;
+	console.log(math);
+}
+function subtract() {
+		// console.log("let's subtract")
+		var math = 7 - 4;
+		console.log(math);
+	}
+function multiply() {
+	// console.log("let's multiply")
+	var math = 4 * 7;
+	console.log(math)
+}
+function divide() {
+	// console.log("let's divide")
+	var math = 50 / 5;
+	console.log(math);
+}
